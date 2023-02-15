33be50d06ca0a0cc837d4af0adaa5d77fee8192b
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-02-14 21:04:58 -0600

    Modelos para el Inventario terminados

---
+++ Models/Inventario/MateriaPrima.cs
@@ -0,0 +1,20 @@
+﻿using Models.Interfaces;
+
+namespace Models.Inventario
+{
+    /// <summary>
+    /// Objeto simple de tipo Materia Prima.
+    /// </summary>
+    public class MateriaPrima : IIdentity, IProduct, IActivable
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+        
+        public decimal Precio { get; set; }
+        
+        public double Cantidad { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
---
+++ Models/Inventario/Producto.cs
@@ -0,0 +1,25 @@
+﻿using Models.Interfaces;
+
+namespace Models.Inventario
+{
+    /// <summary>
+    /// Objeto simple de tipo Producto.
+    /// </summary>
+    public class Producto : IIdentity, IProduct, IActivable
+    {
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public double Cantidad { get; set; }
+
+        public bool Estado { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el identificador único de la Categoría de Producto a la que está asociado.
+        /// </summary>
+        public int IdCategoria { get; set; }
+    }
+}
