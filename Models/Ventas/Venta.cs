fbc43b403589851536e8cd4cf0d2b095a3bc6b3d
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-02-17 21:28:16 -0600

    Todos los modelos han sido terminados

---
+++ Models/Ventas/Venta.cs
@@ -0,0 +1,24 @@
+﻿using Models.Interfaces;
+using System;
+
+namespace Models.Ventas
+{
+    /// <summary>
+    /// Objeto simple de tipo Venta.
+    /// </summary>
+    public class Venta : IIdentity, ITransaction, IActivable
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public bool Estado { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el identificador único del Cliente asociado a la Venta.
+        /// </summary>
+        public int IdCliente { get; set; }
+
+        public int IdEmpleado { get; set; }
+    }
+}
