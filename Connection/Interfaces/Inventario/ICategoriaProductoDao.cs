6b41a7e0e4f2f25b1c5b5994d5c030d818e9d9b5
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-02-19 21:44:00 -0600

    Se agregaron las interfaces DAO para el esquema Inventario

---
+++ Connection/Interfaces/Inventario/ICategoriaProductoDao.cs
@@ -0,0 +1,26 @@
+﻿using Connection.Base;
+using Models.Inventario;
+using System.Collections.Generic;
+
+namespace Connection.Interfaces.Inventario
+{
+    /// <summary>
+    /// Indica que una clase implementa los métodos para el acceso a datos para las Categoría de Productos.
+    /// </summary>
+    public interface ICategoriaProductoDao : IDao<CategoriaProducto>
+    {
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar un registro que coincida con el ID a filtrar.
+        /// </summary>
+        /// <param name="id">Identificador único de la Categoría de Producto a buscar.</param>
+        /// <returns>Objeto de tipo Categoría de Producto que resulte de la búsqueda; en caso de no encontrar, retorna nulo.</returns>
+        CategoriaProducto GetById(int id);
+
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar una serie de registros que coincidan con el valor a filtrar.
+        /// </summary>
+        /// <param name="value">Valor a filtrar la búsqueda. Puede ser por Nombre o Descripción.</param>
+        /// <returns>Colección de objetos de tipo Categoría de Producto que resulten de la búsqueda; en caso de no encontrar, retorna una colección vacía.</returns>
+        IEnumerable<CategoriaProducto> Read(string value);
+    }
+}
---
+++ Connection/Interfaces/Inventario/IMateriaPrimaDao.cs
@@ -0,0 +1,26 @@
+﻿using Connection.Base;
+using Models.Inventario;
+using System.Collections.Generic;
+
+namespace Connection.Interfaces.Inventario
+{
+    /// <summary>
+    /// Indica que una clase implementa los métodos de acceso a datos para las Materias Primas.
+    /// </summary>
+    public interface IMateriaPrimaDao : IDao<MateriaPrima>
+    {
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar un registro que coincida con el ID a filtrar.
+        /// </summary>
+        /// <param name="id">Identificador único para la Materia Prima a buscar.</param>
+        /// <returns>Objeto de tipo Materia Prima que resulte de la búsqueda; en caso de no encontrar, retorna nulo.</returns>
+        MateriaPrima GetById(int id);
+
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar una colección de registros que coincidan con la descripción a filtrar.
+        /// </summary>
+        /// <param name="descripcion">Descripción a buscar.</param>
+        /// <returns>Colección de objetos de tipo Materia Prima que resulten de la búsqueda; en caso de no encontrar, retorna una colección vacía.</returns>
+        IEnumerable<MateriaPrima> Read(string descripcion);
+    }
+}
