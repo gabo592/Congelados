38a2972b9e328b43449b6f4401e542f1e5119fae
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-02-22 12:32:51 -0600

    Se agregaron las interfaces DAO para los esquemas Seguridad y Ubicacion

---
+++ Connection/Interfaces/Ubicacion/IDepartamentoDao.cs
@@ -0,0 +1,26 @@
+﻿using Connection.Base;
+using Models.Ubicacion;
+using System.Collections.Generic;
+
+namespace Connection.Interfaces.Ubicacion
+{
+    /// <summary>
+    /// Indica que una clase implementa los métodos de acceso a datos para los Departamentos.
+    /// </summary>
+    public interface IDepartamentoDao : IDao<Departamento>
+    {
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar un registro que coincida con el ID a filtrar.
+        /// </summary>
+        /// <param name="id">Identificador único del Departamento a buscar.</param>
+        /// <returns>Objeto de tipo Departamento que resulte de la búsqueda; en caso de no encontrar, retorna nulo.</returns>
+        Departamento GetById(int id);
+
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar una colección de registros que coincidan con el Nombre a filtrar.
+        /// </summary>
+        /// <param name="nombre">Nombre del Departamento a filtrar.</param>
+        /// <returns>Colección de objetos de tipo Departamento que resulten de la búsqueda; en caso de no encontrar, retorna una colección vacía.</returns>
+        IEnumerable<Departamento> Read(string nombre);
+    }
+}
---
+++ Connection/Interfaces/Ubicacion/IMunicipioDao.cs
@@ -0,0 +1,33 @@
+﻿using Connection.Base;
+using Models.Ubicacion;
+using System.Collections.Generic;
+
+namespace Connection.Interfaces.Ubicacion
+{
+    /// <summary>
+    /// Indica que una clase implementa los métodos de acceso a datos para los Municipios.
+    /// </summary>
+    public interface IMunicipioDao : IDao<Municipio>
+    {
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar un registro que coincida con el ID a filtrar.
+        /// </summary>
+        /// <param name="id">Identificador único del Municipio a buscar.</param>
+        /// <returns>Objeto de tipo Municipio que resulte de la búsqueda; en caso de no encontrar, retorna nulo.</returns>
+        Municipio GetById(int id);
+
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar una colección de registros que coincidan con el Nombre a filtrar.
+        /// </summary>
+        /// <param name="nombre">Nombre del Municipio a buscar.</param>
+        /// <returns>Colección de objetos de tipo Municipio que resulten de la búsqueda; en caso de no encontrar, retorna una colección vacía.</returns>
+        IEnumerable<Municipio> Read(string nombre);
+
+        /// <summary>
+        /// Realiza una búsqueda dentro de la base de datos hasta encontrar una colección de registros que coincidan con el ID del Departamento a filtrar.
+        /// </summary>
+        /// <param name="idDepartamento">Identificador único del Departamento asociado a los Municipios a buscar.</param>
+        /// <returns>Colección de objetos de tipo Municipio que resulten de la búsqueda; en caso de no encontrar, retorna una colección vacía.</returns>
+        IEnumerable<Municipio> GetByIdDepartamento(int idDepartamento);
+    }
+}
