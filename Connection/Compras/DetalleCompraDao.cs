f40f78830170732ed5fd29c4a0275512158fc77d
Gabriel Aviles <gabrielaviles240@gmail.com>
2023-02-24 17:35:33 -0600

    Se agregaron los DAO para el esquema Compras

---
+++ Connection/Compras/DetalleCompraDao.cs
@@ -0,0 +1,119 @@
+﻿using Common.Utils;
+using Connection.Base;
+using Connection.Constantes;
+using Connection.Interfaces.Compras;
+using Models.Compras;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Connection.Compras
+{
+    /// <summary>
+    /// Clase encargada de implementar los métodos de acceso a datos para los Detalles de Compra.
+    /// </summary>
+    internal class DetalleCompraDao : BaseDao<DetalleCompra>, IDetalleCompraDao
+    {
+        /// <summary>
+        /// Administrador de errores.
+        /// </summary>
+        private readonly ErrorHandler Handler;
+
+        public DetalleCompraDao(string connectionString, ErrorHandler handler) : base(connectionString, handler)
+        {
+            Handler = handler;
+        }
+
+        public override DetalleCompra Create(DetalleCompra model)
+        {
+            if (Validations.HasError(model, Handler, Validations.Operations.CREATE)) return null;
+
+            return Read(StoredProcedures.DetalleCompraCreate, new Dictionary<string, object>
+            {
+                {"IdCompra", model.IdCompra },
+                {"IdMateriaPrima", model.IdMateriaPrima },
+                {"Costo", model.Costo },
+                {"Cantidad", model.Cantidad },
+                {"Descuento", model.Descuento }
+            }).FirstOrDefault();
+        }
+
+        public override DetalleCompra Delete(DetalleCompra model)
+        {
+            if (Validations.HasError(model, Handler, Validations.Operations.DELETE)) return null;
+
+            return Read(StoredProcedures.DetalleCompraDelete, new Dictionary<string, object>
+            {
+                {"IdCompra", model.IdCompra },
+                {"IdMateriaPrima", model.IdMateriaPrima }
+            }).FirstOrDefault();
+        }
+
+        public IEnumerable<DetalleCompra> GetByIdCompra(int idCompra)
+        {
+            if (idCompra.Equals(default) || idCompra < 0)
+            {
+                Handler.Add("ID_COMPRA_DEFAULT");
+                return Enumerable.Empty<DetalleCompra>();
+            }
+
+            return Read(StoredProcedures.DetalleCompraRead, new Dictionary<string, object>
+            {
+                {"IdCompra", idCompra }
+            });
+        }
+
+        public IEnumerable<DetalleCompra> GetByIdMateriaPrima(int idMateriaPrima)
+        {
+            if (idMateriaPrima.Equals(default) || idMateriaPrima < 0)
+            {
+                Handler.Add("ID_MATERIA_PRIMA_DEFAULT");
+                return Enumerable.Empty<DetalleCompra>();
+            }
+
+            return Read(StoredProcedures.DetalleCompraRead, new Dictionary<string, object>
+            {
+                {"IdMateriaPrima", idMateriaPrima }
+            });
+        }
+
+        public override IEnumerable<DetalleCompra> Read()
+        {
+            return Read(StoredProcedures.DetalleCompraRead);
+        }
+
+        public DetalleCompra Read(int idCompra, int idMateriaPrima)
+        {
+            if (idCompra.Equals(default) || idCompra < 0)
+            {
+                Handler.Add("ID_COMPRA_DEFAULT");
+                return null;
+            }
+
+            if (idMateriaPrima.Equals(default) || idMateriaPrima < 0)
+            {
+                Handler.Add("ID_MATERIA_PRIMA_DEFAULT");
+                return null;
+            }
+
+            return Read(StoredProcedures.DetalleCompraRead, new Dictionary<string, object>
+            {
+                {"IdCompra", idCompra },
+                {"IdMateriaPrima", idMateriaPrima }
+            }).FirstOrDefault();
+        }
+
+        public override DetalleCompra Update(DetalleCompra model)
+        {
+            if (Validations.HasError(model, Handler, Validations.Operations.UPDATE)) return null;
+
+            return Read(StoredProcedures.DetalleCompraUpdate, new Dictionary<string, object>
+            {
+                {"IdCompra", model.IdCompra },
+                {"IdMateriaPrima", model.IdMateriaPrima },
+                {"Costo", model.Costo },
+                {"Cantidad", model.Cantidad },
+                {"Descuento", model.Descuento }
+            }).FirstOrDefault();
+        }
+    }
+}
--- Connection/Constantes/StoredProcedures.cs
+++ Connection/Constantes/StoredProcedures.cs
@@ -13,5 +13,25 @@
         public const string EmpleadoDelete = "CapitalHumano.EMPLEADO_DELETE";
 
         #endregion
+
+        #region Compras
+
+        public const string CompraCreate = "Compras.COMPRA_CREATE";
+        public const string CompraRead = "Compras.COMPRA_READ";
+        public const string CompraReadByDates = "Compras.COMPRA_READ_BY_DATES";
+        public const string CompraUpdate = "Compras.COMPRA_UPDATE";
+        public const string CompraDelete = "Compras.COMPRA_DELETE";
+
+        public const string DetalleCompraCreate = "Compras.DETALLE_COMPRA_CREATE";
+        public const string DetalleCompraRead = "Compras.DETALLE_COMPRA_READ";
+        public const string DetalleCompraUpdate = "Compras.DETALLE_COMPRA_UPDATE";
+        public const string DetalleCompraDelete = "Compras.DETALLE_COMPRA_DELETE";
+
+        public const string ProveedorCreate = "Compras.PROVEEDOR_CREATE";
+        public const string ProveedorRead = "Compras.PROVEEDOR_READ";
+        public const string ProveedorUpdate = "Compras.PROVEEDOR_UPDATE";
+        public const string ProveedorDelete = "Compras.PROVEEDOR_DELETE";
+
+        #endregion
     }
 }
